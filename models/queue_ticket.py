5ee829bd3707b149ab104a72cb51e3f202abc281
jardel.bernardo <jardel.bernardo@alien-group.com>
2025-08-31 23:08:38 +0100

    [UPD] onchange the name of a ticket when select the service
    Took 23 minutes

--- models/queue_ticket.py
+++ models/queue_ticket.py
@@ -1,13 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class QueueTicket(models.Model):
     _name = 'queue.ticket'
     _description = 'Queue Management Ticket'
 
-    name = fields.Char("Name")
-    client_id = fields.Many2one('res.partner')
-    service_id = fields.Many2one('queue.service')
-    ticket_number = fields.Char('') # automatic sequence
+    name = fields.Char("Name", required=True)
+    client_id = fields.Many2one('res.partner', required=True, domain=[('is_company','=', False)])
+    service_id = fields.Many2one('queue.service', require=True)
+    ticket_number = fields.Char('Number') # automatic sequence
     state = fields.Selection([
         ('draft', 'Draft'),('waiting', 'Waiting'),
         ('called', 'Called'),('done', 'Done'),
@@ -17,4 +17,6 @@ class QueueTicket(models.Model):
     qrcode = fields.Char('Qrcode')
 
 
-
+    @api.onchange('service_id')
+    def _onchange_service(self):
+        self.name = self.service_id.name
